f3b74f2a77023834ca89804a3b69093c819e00bd
gustonstomy <gustomstom@gmail.com>
2023-11-28 12:46:15 -0800

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,9 +10,9 @@ if last_digit < 6:
     if last_digit == 0:
         print(f"Last digit of {number} is {last_digit} and is 0")
     else:
-        print(f"Last digit of {number} is {last_digit}"
-        " and is less than 6 and not 0")
+        print(f"Last digit of {number} is {last_digit} ")
+        ("and is less than 6 and not 0")
 elif last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
-    print(f"Last digit of 0 is 0 and is 0")
+    print(f"Last digit of {number} is {last_digit} and is 0")
