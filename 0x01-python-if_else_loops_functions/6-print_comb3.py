2c6c8956661d4db410ddd51e235d2ab65d931ba0
gustonstomy <gustomstom@gmail.com>
2023-11-28 13:49:01 -0800

    advanced

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for i in range(1, 100):
     if i < 10:
         first_digit = 0
         second_digit = i
-    else :
+    else:
         first_digit = i // 10
         second_digit = i % 10
     if first_digit < second_digit and int(f"{second_digit}{first_digit}") > i:
