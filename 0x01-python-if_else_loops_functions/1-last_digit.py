161f2030fefe762acdb71dc03f2b1554800d65e6
gustonstomy <gustomstom@gmail.com>
2023-11-28 12:03:47 -0800

    Readme

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    last_digit = -int(str(number)[-1])
+if number > 0:
+    last_digit = int(str(number)[-1])
+
+if last_digit < 6:
+    if last_digit == 0:
+        print(f"Last digit of {number} is {last_digit} and is 0")
+    else: print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+elif number > 5:
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for i in range(99):
+    hez = hex_representation = hex(i)
+    print(f"{i} = {hez}")
