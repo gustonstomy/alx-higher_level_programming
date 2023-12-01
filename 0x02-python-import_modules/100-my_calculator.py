6b6eeeb1c22ed8b147aa0d111de786d31e1281f0
gustonstomy <gustomstom@gmail.com>
2023-12-01 01:41:48 -0800

    corrections to 102-magic_calculation.py,101-easy_print.py,100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    from add_0 import add, sub, mul, div
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
 
-    length = len(sys.argv)
+    length = len(argv)
     if length < 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif length > 3 and sys.argv[2] not in {'+', '-', '/', '*'}:
+    elif length > 3 and argv[2] not in {'+', '-', '/', '*'}:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        operator = sys.argv[2]
+        a = int(argv[1])
+        b = int(argv[3])
+        operator = argv[2]
         if operator == '+':
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
         elif operator == '-':
@@ -25,4 +25,4 @@ if __name__ == "__main__":
                 print("Division by zero is not allowed.")
             else:
                 print("{:d} / {:d} = {:.2f}".format(a, b, div(a, b)))
-                sys.exit(1)
+                exit(1)
