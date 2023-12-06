a9b025e41f106e0fe6f95da0a2f54f3c6f2d1ea2
gustonstomy <gustomstom@gmail.com>
2023-12-06 05:16:21 -0800

    from 8 to 12

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     
-    if not sentence:
+    if len(sentence) == 0:
         return None
     else:
         return len(sentence), sentence[0]
