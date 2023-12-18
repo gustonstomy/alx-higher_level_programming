8d63e11acacc1b23f7a283d9268957127c1f6f13
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-12-18 18:20:49 +0000

    Update 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,6 +16,6 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
             print("out of range")
         finally:
-            pa
+            pass
         div.append(result)
     return div
