1113482ecd654b662f1734807fb8c1d08fa640ae
gustonstomy <gustomstom@gmail.com>
2023-12-19 12:15:10 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+class definition
+"""
+
+
+class Square:
+    """
+    private class instance attribute size
+    """
+
+    def __init__(self, size=0):
+        """
+        Args:
+            size: the size of square
+        """
+        self.__size = size
+
